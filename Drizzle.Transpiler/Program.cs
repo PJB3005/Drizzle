12613d7b07f9b81f176659761cd91cfc39d9fe2c
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-07-08 15:04:08 +0200

    Create Drizzle.Transpiler project.

---
+++ Drizzle.Transpiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Drizzle.Transpiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
