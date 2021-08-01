88be2598ecc534f7478ef97a29c77472c6b3c547
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-08-02 00:20:19 +0200

    Implement fileIO xtra.

--- Drizzle.Lingo.Runtime/Xtra/FileIOXtra.cs
+++ Drizzle.Lingo.Runtime/Xtra/FileIOXtra.cs
@@ -1,8 +1,38 @@
-﻿namespace Drizzle.Lingo.Runtime.Xtra
+﻿using System;
+using System.IO;
+
+namespace Drizzle.Lingo.Runtime.Xtra
 {
     public sealed class FileIOXtra : BaseXtra
     {
+        private FileStream? _file;
+        private StreamReader? _reader;
 
+        public void openfile(string filePath, int mode = 0)
+        {
+            var access = mode switch
+            {
+                0 => FileAccess.ReadWrite,
+                1 => FileAccess.Read,
+                2 => FileAccess.Write,
+                _ => throw new ArgumentException("Invalid file mode!")
+            };
+
+            _file = File.Open(filePath, FileMode.Open, access);
+            _reader = new StreamReader(_file, leaveOpen: true);
+        }
+
+        public void closefile()
+        {
+            _file?.Dispose();
+            _file = null;
+            _reader = null;
+        }
+
+        public string readfile()
+        {
+            return _reader!.ReadToEnd();
+        }
 
         public override BaseXtra Duplicate()
         {
