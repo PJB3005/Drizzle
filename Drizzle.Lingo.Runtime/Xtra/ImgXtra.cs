ffe152e0e591d68af3b66691c74ffffa0aa011f7
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-08-02 18:47:21 +0200

    Implement imgxtra

--- Drizzle.Lingo.Runtime/Xtra/ImgXtra.cs
+++ Drizzle.Lingo.Runtime/Xtra/ImgXtra.cs
@@ -1,10 +1,21 @@
-﻿namespace Drizzle.Lingo.Runtime.Xtra
+﻿using SixLabors.ImageSharp;
+
+namespace Drizzle.Lingo.Runtime.Xtra
 {
     public sealed class ImgXtra : BaseXtra
     {
         public override BaseXtra Duplicate()
         {
             return new ImgXtra();
+        }
+
+        public int ix_saveimage(LingoPropertyList props)
+        {
+            var img = (LingoImage)props["image"]!;
+            var fileName = (string)props["filename"]!;
+
+            img.Image.SaveAsPng(fileName);
+            return 1;
         }
     }
 }
