97b19b285e81248cd6c04741731fe028515e76ee
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-08-09 14:26:54 +0200

    Add first of many copypixels() tests.

---
+++ Drizzle.Lingo.Tests/LingoImageTest.cs
@@ -0,0 +1,51 @@
+﻿using Drizzle.Lingo.Runtime;
+using NUnit.Framework;
+
+namespace Drizzle.Lingo.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    [TestOf(typeof(LingoImage))]
+    public sealed class ImageTest
+    {
+        [Test]
+        public void TestCopyPixelsMove()
+        {
+            // Set up source image to have a blue-bordered red/green checkerboard in the center.
+            // Then copy the shape to another image.
+            var src = new LingoImage(128, 128, 32);
+            var dst = new LingoImage(128, 128, 32);
+
+            for (var y = 32; y < 96; y++)
+            {
+                for (var x = 32; x < 96; x++)
+                {
+                    if (x == 32 || x == 95 || y == 32 || y == 95)
+                    {
+                        src.setpixel(x, y, new LingoColor(0, 0, 255));
+                    }
+                    else
+                    {
+                        var color = x % 2 == 0 ^ y % 2 == 0 ? new LingoColor(255, 0, 0) : new LingoColor(0, 255, 0);
+                        src.setpixel(x, y, color);
+                    }
+                }
+            }
+
+            // copy
+            dst.copypixels(src, new LingoRect(16, 16, 80, 80), new LingoRect(32, 32, 96, 96));
+
+            // assert
+            for (var y = 0; y < 64; y++)
+            {
+                for (var x = 0; x < 64; x++)
+                {
+                    var srcPx = src.getpixel(x + 32, y + 32);
+                    var dstPx = dst.getpixel(x + 16, y + 16);
+
+                    Assert.That(srcPx, Is.EqualTo(dstPx));
+                }
+            }
+        }
+    }
+}
