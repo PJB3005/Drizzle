a4750097f40f3396e07dd441c18b75d83e232fb1
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-12-07 22:55:46 +0100

    Wrap string tostring in string

--- Drizzle.Lingo.Runtime/Data/LingoFormat.cs
+++ Drizzle.Lingo.Runtime/Data/LingoFormat.cs
@@ -5,7 +5,7 @@ public static class LingoFormat
     public static string LingoToString(object? obj)
     {
         if (obj is string str)
-            return $"{str}";
+            return $"\"{str}\"";
 
         return obj?.ToString() ?? "<Void>";
     }
