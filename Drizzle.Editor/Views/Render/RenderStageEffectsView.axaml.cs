58985ccef8e031b27fed1659399f01657d4913f1
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-12-25 03:51:09 +0100

    Use avalonia bool-based XAML styles in effects renders.
    Now that Rider seems to support them.

--- Drizzle.Editor/Views/Render/RenderStageEffectsView.axaml.cs
+++ Drizzle.Editor/Views/Render/RenderStageEffectsView.axaml.cs
@@ -12,25 +12,5 @@ public partial class RenderStageEffectsView : UserControl
     public RenderStageEffectsView()
     {
         InitializeComponent();
-
-        this.FindControl<ItemsControl>("EffectsList").ItemTemplate = new FuncDataTemplate(
-            typeof(RenderSingleEffectViewModel),
-            (o, _) =>
-            {
-                var effect = (RenderSingleEffectViewModel)o;
-                var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
-
-                if (effect.Current)
-                    stackPanel.Classes.Add("Current");
-
-                stackPanel.Children.Add(new TextBlock { Text = effect.Name });
-
-                return stackPanel;
-            });
-    }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
     }
 }
