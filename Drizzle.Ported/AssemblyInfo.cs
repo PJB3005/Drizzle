53a2384dfc2cb444b205434191e676565e59dc34
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-12-26 17:05:34 +0100

    SkipLocalsInit

--- Drizzle.Ported/AssemblyInfo.cs
+++ Drizzle.Ported/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 
 // WHEE I throw the magic performance word and hope my code is blessed to go ever so slightly faster.
 [module: SkipLocalsInit]
-[assembly: InternalsVisibleTo("Drizzle.Lingo.Tests")]
+
+
